683618e660118967e13ea5372a4dfba06ae8d562
pajama <99984344+pajama10000@users.noreply.github.com>
2023-07-12 14:16:48 -0400

    Update and rename CrystalOptimizer.java to CrystalOptimizer.kt

--- common/src/main/java/io/github/cdagaming/crystaloptimizer/CrystalOptimizer.java
+++
@@ -1,9 +0,0 @@
-package io.github.cdagaming.crystaloptimizer;
-
-public class CrystalOptimizer {
-    public static final String MOD_ID = "crystaloptimizer";
-
-    public static void init() {
-        // N/A
-    }
-}
---
+++ common/src/main/java/io/github/cdagaming/crystaloptimizer/CrystalOptimizer.kt
@@ -0,0 +1,9 @@
+package io.github.cdagaming.crystaloptimizer
+
+object CrystalOptimizer {
+    const val MOD_ID = "crystaloptimizer"
+
+    fun init() {
+        // N/A
+    }
+}
